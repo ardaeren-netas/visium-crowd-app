f5c884ba0f974971175ac4136af49698a5f32721
Arda Eren <aeren@netas.com.tr>
2021-01-13 01:34:50 +0300

    article of day componenti düzeltildi

--- src/components/Dashboard/articleOfDay.tsx
+++ src/components/Dashboard/articleOfDay.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const articleOfDay = () => {
   return (
-    <div className="bg-red-700 grid-cols-2 shadow-lg rounded-xl p-10">
+    <div className=" flex flex-col justify-start bg-red-700 shadow-lg m-5 rounded-xl p-6 w-64">
       <h1 className="text-white font-sans pb-3">Article of Day</h1>
 
-      <h1 className=" text-3xl font-bold text-white col-span-2">
+      <h1 className=" text-3xl font-bold text-white">
         How testing can Improve User expereince
       </h1>
 
-      <a className="text-white text-right" href="#">
+      <a className="justify-self-end text-white text-right pt-3" href="#">
         Discover now
       </a>
     </div>
