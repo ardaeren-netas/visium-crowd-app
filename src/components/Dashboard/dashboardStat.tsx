5406a98f4d35298804493dae0f1fbac385144482
Arda Eren <aeren@netas.com.tr>
2021-01-12 22:51:05 +0300

    dashboard single stat counter pading düzenlendi

--- src/components/Dashboard/dashboardStat.tsx
+++ src/components/Dashboard/dashboardStat.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const DashboardStat = () => {
   return (
     <div className="bg-gradient-to-r from-purple-800 to-purple-600 rounded-xl px-5 py-4 w-60">
-      <div className="font-sans text-base text-white "> Draft Issues</div>
-      <div className="font-sans text-white text-4xl font-bold"> 5</div>
+      <div className="font-sans text-base text-white pb-1 "> Draft Issues</div>
+      <div className="font-sans text-white text-4xl font-bold pb-1"> 5</div>
       <div className="font-sans text-white text-xs font-xs">
         {" "}
         Lorem ipsum dolor sit amet
