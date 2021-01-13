097d8acf276fcdf889672dcc67e0b0695e9e739b
ardaeren-netas <76147924+ardaeren-netas@users.noreply.github.com>
2021-01-13 17:09:02 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        turkuaz: {
-          light: "#1aa6af",
-          DEFAULT: "#009ca6",
-          dark: "#004e53",
-        },
-      },
+    fontfamily: {
+      sans: ["Inter", "Arial", "sans-serif"],
     },
+    extend: {},
   },
   variants: {
-    extend: {
-      backgroundColor: ["active"],
-      textColor: ["active"],
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
